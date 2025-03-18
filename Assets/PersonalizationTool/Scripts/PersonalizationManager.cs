bbd71ac6f95daa9685a1c5a62b6f4bc88d58437b
82L <203681278+82l-lsgroup@users.noreply.github.com>
2025-03-18 11:14:51 +0100

    corrected personalization manager

--- Assets/PersonalizationTool/Scripts/PersonalizationManager.cs
+++ Assets/PersonalizationTool/Scripts/PersonalizationManager.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Threading.Tasks;
 using Gamification.Help;
+using Gamification.View;
 using PersonalizationTool.Redis;
 using UnityEngine;
 using XdeEngine.Assembly;
@@ -22,35 +23,53 @@ namespace PersonalizationTool.Scripts
 		private string m_redisServerPort;
 		
 		[SerializeField]
-		//[Tooltip("0 = easy, 1 = medium, 2 = hard")]
+		[Tooltip("0 = easy, 1 = medium, 2 = hard")]
 		private int m_activityLevel = 1;
 
 		[SerializeField]
-		//[Tooltip("0 = novice, 1 = medium, 2 = expert")]
+		[Tooltip("0 = novice, 1 = confirmed, 2 = expert")]
 		private int m_userLevel = 1;
 
 		[SerializeField]
 		private HelpController m_helpController;
 
-		// private RedisManager m_redisManager;
+		[SerializeField]
+		private StartupScreen m_startupScreen;
+		
+		[SerializeField]
+		private XdeAsbScenario m_xdeAsbScenario;
+		private RedisManager m_redisManager;
+
+		private int m_currentActivity = 0;
 
 		private void Awake()
 		{
 			m_helpController.OnNextStepAction += OnNextStepAction;
-			// m_redisManager = new RedisManager();
-			// if (!Application.isEditor)
-			// {
-			// 	m_redisManager.SetConnectionData(m_redisServerIp, m_redisServerPort);
-			// }
-			// m_redisManager.NewNextActivityData += OnNewNextActivityData;
+			m_startupScreen.StartScenarioAction += SetupActivityLevel;
+			
+			m_redisManager = new RedisManager();
+			if (!Application.isEditor)
+			{
+				m_redisManager.SetConnectionData(m_redisServerIp, m_redisServerPort);
+			}
+			m_redisManager.ConnectRedis();
+			m_redisManager.NewNextActivityData += OnNewNextActivityData;
+			
 		}
 
 		private void OnDestroy()
 		{
-			// m_redisManager.NewNextActivityData -= OnNewNextActivityData;
-		}
+			m_helpController.OnNextStepAction -= OnNextStepAction;
+			m_startupScreen.StartScenarioAction -= SetupActivityLevel;
 
-		
+
+			if (m_currentActivity > 0)
+			{
+				m_redisManager.StopActivity(m_currentActivity);
+			}
+			m_redisManager.NewNextActivityData -= OnNewNextActivityData;
+			m_redisManager.DisconnectRedis();
+		}
 
 		private void OnNewNextActivityData(RedisManager.NextActivityData p_obj)
 		{
@@ -61,6 +80,7 @@ namespace PersonalizationTool.Scripts
 
 		private void SetupActivityLevel()
 		{
+			Debug.Log($"SetActvityLevel: {m_activityLevel}");
 			switch (m_activityLevel)
 			{
 				case 0:
@@ -79,21 +99,35 @@ namespace PersonalizationTool.Scripts
 
 		private void OnNextStepAction()
 		{
-			// Redis send a lot of user frustrated
+			OnStepDeactivated();
+			m_currentActivity = m_xdeAsbScenario.GetActiveStep(0).GetInstanceID();
+			Debug.Log($"SetActivityLevel: {m_currentActivity}");
+			m_activityLevel -= 1;
+			m_redisManager.StartActivity(m_currentActivity, m_activityLevel, m_userLevel);
 		}
 		public void OnStepActivated(XdeAsbStep p_step)
 		{
-			// m_redisManager.StartActivity(p_step.name, 0, m_userLevel);
+			Debug.Log($"OnStepActivated: {p_step.GetInstanceID()}");
+			if (m_currentActivity != 0)
+			{
+				m_redisManager.StopActivity(m_currentActivity);
+			}
+			m_currentActivity = p_step.GetInstanceID();
+			m_redisManager.StartActivity(m_currentActivity, m_activityLevel, m_userLevel);
 		}
 
-		public void OnStepDeactivated(XdeAsbStep p_step)
+		public void OnStepDeactivated()
 		{
-			// m_redisManager.StopActivity(p_step.name);
+			Debug.Log($"OnStepDeactivated: {m_currentActivity}");
+			m_redisManager.StopActivity(m_currentActivity);
+			m_currentActivity = 0;
 		}
 
-		public void SetUserLevel(int p_userLevel)
+		public void SetStartupAppLevel(int p_userLevel)
 		{
+			Debug.Log($"SetStartupAppLevel: {p_userLevel}");
 			m_userLevel = p_userLevel;
+			m_activityLevel = p_userLevel;
 		}
 
 	
